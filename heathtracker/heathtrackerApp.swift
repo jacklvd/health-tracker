77dc8bea7db533228e406a9b1a618b52e9261de8
jacklvd <vodanglong3103@gmail.com>
2025-08-07 00:03:41 -0400

    Initial Commit

---
+++ heathtracker/heathtrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  heathtrackerApp.swift
+//  heathtracker
+//
+//  Created by Jack Vo on 8/7/25.
+//
+
+import SwiftUI
+
+@main
+struct heathtrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
